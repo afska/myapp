a625c83f2d2881e4861ba03f8e14a35f4b4532b0
Rodrigo Alfonso <rodri042@gmail.com>
2021-01-23 22:08:57 -0300

    Replacing nes.css with bootstrap

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "nes.css/css/nes.css";
+import "./gui/theme/bootswatch/theme.css";
+import "./gui/theme/bootswatch/custom.css";
+import "./gui/theme/bootswatch/global.css";
 import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
